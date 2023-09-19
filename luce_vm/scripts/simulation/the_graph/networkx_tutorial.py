7b4da039586d216ba653f6f9ac3b3468774f0f90
Kun Li <likun6034@gmail.com>
2023-09-19 10:53:10 +0200

    Update senario 1 - users share data

--- luce_vm/scripts/simulation/the_graph/networkx_tutorial.py
+++ luce_vm/scripts/simulation/the_graph/networkx_tutorial.py
@@ -1,7 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.Graph(day="Friday")
+DG = nx.DiGraph()
+DG.add_edges_from([(1, 2), (1, 3), (2, 3), (3, 4), (4, 5), (5, 1)])
 
-nx.draw(G, with_labels=True)
+nx.draw(DG, with_labels=True)
 plt.show()
