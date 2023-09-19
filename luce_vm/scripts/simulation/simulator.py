ed5970fbd495676bb7b0f2952ae03e5f717b84cb
Kun Li <likun6034@gmail.com>
2023-09-19 10:53:10 +0200

    Update senario 1 - users share data

--- luce_vm/scripts/simulation/simulator.py
+++ luce_vm/scripts/simulation/simulator.py
@@ -9,186 +9,133 @@ from generate_user import generate_users
 
 luce_django_path = os.path.abspath('../..')  # luce_vm
 luce_django_path = os.path.join(luce_django_path, 'luce_django/luce')
-# print(luce_django_path)
 sys.path.insert(0, luce_django_path)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'lucehome.settings'
-
 django.setup()
 
 from accounts.models import User
-# BASE_DIR = "../../luce_django/luce"
 
-# sys.path.append("../../luce_django/luce")
-# from accounts.models import User
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', "lucehome.settings")
-# # django.setup()
-
-# from accounts.models import User
-
-http = urllib3.PoolManager()
-
-registration_data = {
-    "last_name": "piccini",
-    "email": "email90@email.com",
-    "password": "password123",
-    "create_wallet": True,
-    "user_type": 0
-}
-
-user = {
-    "username": registration_data["email"],
-    "password": registration_data["password"]
-}
-
-url = "http://127.0.0.1:8000/user/login/"
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
-def Login(url, user):
-    encoded_user = json.dumps(user).encode('utf-8')
-    r = http.request('POST',
-                     url,
-                     body=encoded_user,
-                     headers={'Content-Type': 'application/json'})
+class Simulator:
+    def __init__(self, num_of_users):
+        self.http = urllib3.PoolManager()
+        self.urls = {
+            "login": "http://127.0.0.1:8000/user/login/",
+            "register": "http://localhost:8000/user/register/",
+            "upload_data": "http://localhost:8000/contract/dataUpload/",
+            "deploy_registry": "http://localhost:8000/admin/deployRegistry/"
+        }
 
-    token = json.loads(r.data.decode('utf-8'))["data"]["token"]
-    return token
+        self.user = generate_users(num_of_users)
+        self.directed_graph = nx.DiGraph()
 
+        self.switcher = {1: self._senario_1}
 
-registration_url = "http://localhost:8000/user/register/"
+    def run(self, senario_num):
+        self.switcher.get(senario_num, lambda: "Invalid senario")()
 
+    def _login(self, url, user):
+        encoded_user = json.dumps(user).encode('utf-8')
+        r = self.http.request('POST',
+                              self.urls['login'],
+                              body=encoded_user,
+                              headers={'Content-Type': 'application/json'})
 
-def Register(registration_url, registration_data):
-    r = http.request('POST',
-                     registration_url,
-                     body=json.dumps(registration_data).encode('utf-8'),
-                     headers={'Content-Type': 'application/json'})
+        token = json.loads(r.data.decode('utf-8'))["data"]["token"]
+        return token
 
-    result = json.loads(r.data.decode('utf-8'))
-    # print(result)
-    return result
+    def _register(self, registration_url, registration_data):
+        r = self.http.request(
+            'POST',
+            self.urls['register'],
+            body=json.dumps(registration_data).encode('utf-8'),
+            headers={'Content-Type': 'application/json'})
 
+        result = json.loads(r.data.decode('utf-8'))
+        # print(result)
+        return result
 
-# Register(registration_url, registration_data)
-# token = Login(url, user)
-# print(token)
+    def _upload_data(self, upload_url, data, token):
+        d = json.dumps(data).encode('utf-8')
 
-uploaded_data = {
-    "estimate": False,
-    "description": "ds",
-    "link": "http://link.com",
-    "no_restrictions": False,
-    "open_to_general_research_and_clinical_care": False,
-    "open_to_HMB_research": False,
-    "open_to_population_and_ancestry_research": False,
-    "open_to_disease_specific": False
-}
-upload_data_url = "http://localhost:8000/contract/dataUpload/"
+        r = self.http.request('POST',
+                              self.urls['upload_data'],
+                              body=d,
+                              headers={
+                                  'Content-Type': 'application/json',
+                                  'Authorization': 'Token ' + token
+                              })
 
+        result = json.loads(r.data.decode('utf-8'))
+        # print(result)
+        return result
 
-def UploadData(upload_url, data, token):
-    d = json.dumps(data).encode('utf-8')
+    def _deploy_registry(self, url, admin_token):
+        print("Deploy registry")
+        r = self.http.request("POST",
+                              self.urls['deploy_registry'],
+                              headers={
+                                  'Content-Type': 'application/json',
+                                  'Authorization': 'Token ' + admin_token
+                              })
+        result = json.loads(r.data.decode('utf-8'))
 
-    r = http.request('POST',
-                     upload_data_url,
-                     body=d,
-                     headers={
-                         'Content-Type': 'application/json',
-                         'Authorization': 'Token ' + token
-                     })
+    def _address_to_label(self, address):
+        return address[:5] + "..." + address[-5:]
 
-    result = json.loads(r.data.decode('utf-8'))
-    # print(result)
-    return result
+    def _senario_1(self):
+        print("Start simulate senario 1")
+        # print(self.user)
 
+        # 1. clear user data
+        User.objects.all().delete()
 
-deploy_registry_url = "http://localhost:8000/admin/deployRegistry/"
+        # 2. register users
+        for user in self.user['users']:
+            email = user['registration_data']['email']
+            print("Register user: " + email)
+            self._register(self.urls['register'], user['registration_data'])
 
-
-def DeployRegistry(url, admin_token):
-    print("Deploy registry")
-    r = http.request("POST",
-                     url,
-                     headers={
-                         'Content-Type': 'application/json',
-                         'Authorization': 'Token ' + admin_token
-                     })
-
-    result = json.loads(r.data.decode('utf-8'))
-    # print(result)
-
-
-# UploadData(upload_data_url, uploaded_data)
-
-test_url = "http://localhost:8000/"
-
-
-def Test(url):
-    r = http.request("GRT", url)
-    result = json.loads(r.data.decode('utf-8'))
-    print(result)
-
-
-def pipeline():
-
-    # Test(test_url)
-    # return
-    print("Start simulation")
-    # return
-
-    # 1. clear user data
-    User.objects.all().delete()
-
-    # 2. load faked user data
-    # with open("faked_data.json", "r") as f:
-    #     data = json.load(f)
-
-    data = generate_users(3)
-    # print(data)
-
-    users = data['users']
-
-    # 3. register users
-    for user in users:
-        email = user['registration_data']['email']
-        print("Register user: " + email)
-        Register(registration_url, user['registration_data'])
-
-        # user = User.objects.get(email=email)
-        # print(user.ethereum_public_key)
-
-    # 4. deploy registry
-    admin = users.pop()
-    admin_token = Login(
-        url, {
-            'username': admin['registration_data']['email'],
-            'password': admin['registration_data']['password']
-        })
-    DeployRegistry(deploy_registry_url, admin_token)
-
-    # return
-
-    # 4. upload data
-    print("Upload data:")
-    for user in users:
-        # 4.1 login
-        token = Login(
-            url, {
-                'username': user['registration_data']['email'],
-                'password': user['registration_data']['password']
+        # 3. deploy registry
+        admin = self.user['users'].pop()
+        admin_token = self._login(
+            self.urls['login'], {
+                'username': admin['registration_data']['email'],
+                'password': admin['registration_data']['password']
             })
+        self._deploy_registry(self.urls['deploy_registry'], admin_token)
 
-        email = user['registration_data']['email']
-        user_instance = User.objects.get(email=email)
-        user_address = user_instance.ethereum_public_key
+        # 4. upload data
+        print("Upload data:")
+        for user in self.user['users']:
+            # 4.1 login
+            token = self._login(
+                self.urls['login'], {
+                    'username': user['registration_data']['email'],
+                    'password': user['registration_data']['password']
+                })
 
-        # 4.2 upload data
-        uploaded = UploadData(upload_data_url, user['uploaded_data'], token)
-        print("Upload data for user: " + email)
-        uploaded_address = uploaded['data']['contracts']
-        print("Uploaded address: ")
-        # print(type(uploaded_address))
-        print(uploaded_address['contract_address'])
+            email = user['registration_data']['email']
+            user_instance = User.objects.get(email=email)
+            user_address = user_instance.ethereum_public_key
+
+            # 4.2 upload data
+            uploaded = self._upload_data(self.urls['upload_data'],
+                                         user['uploaded_data'], token)
+            print("Upload data for user: " + email)
+            uploaded_address = uploaded['data']['contracts']
+            print("Uploaded address: ")
+            print(uploaded_address['contract_address'])
+            self.directed_graph.add_edge(
+                self._address_to_label(user_address),
+                self._address_to_label(uploaded_address['contract_address']))
+
+        nx.draw(self.directed_graph, with_labels=True)
+        plt.show()
 
 
-pipeline()
+s = Simulator(3)
+s.run(1)
