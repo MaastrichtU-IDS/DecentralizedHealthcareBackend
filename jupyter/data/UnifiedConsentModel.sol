fd84a7b40411ac76cbfcdc7cf908b2fbcfb2e06d
Dong Yang <dongyang-cs@outlook.com>
2023-11-19 20:30:08 +0100

    disease using mapping to check status

--- jupyter/data/UnifiedConsentModel.sol
+++ jupyter/data/UnifiedConsentModel.sol
@@ -17,9 +17,11 @@ contract ConsentCode {
         uint16 Start_Day;
         uint16 Months;
         uint16[] Disease_Code_Array;
-        uint64 Group_Code;
-        uint8[] Country_Code;
-        uint64[] Country_Group_Code;
+        uint64 Area_Group_Code;
+        uint8[] Area_Country_Code;
+        uint64[] Area_Country_Group_Code;
+        mapping(uint8 => bool) Area_Country_Code_Map;
+        mapping(uint16 => bool) Disease_Map;
     }
 
     mapping(address => Terms) providerMapping; // data subject
@@ -28,19 +30,22 @@ contract ConsentCode {
     address[] DataSubjectAcc;
     address[] DataRequesterAcc;
 
+    uint8 role_provider = 1;
+    uint8 role_requester = 2;
+
     //    role 1 provider,2 requester
 
     function TermsByRole(
-        address _address,
-        uint8 role
+        uint8 role,
+        address _address
     ) internal view returns (Terms storage) {
-        if (role == 1) {
-            require(msg.sender == dataProvider);
+        if (role == role_provider) {
+            // require(msg.sender == dataProvider);
             return providerMapping[_address];
-        } else if (role == 2) {
+        } else if (role == role_requester) {
             return requesterMapping[_address];
         } else {
-            revert("Invalid role");
+            revert("TermsByRole: Invalid role specified");
         }
     }
 
@@ -49,89 +54,91 @@ contract ConsentCode {
         address _address,
         uint16 Simple_Items
     ) public {
-        Terms storage terms = TermsByRole(_address, role);
+        Terms storage terms = TermsByRole(role, _address);
         terms.Simple_Items = Simple_Items;
     }
 
-    function Display(
+    function DisplaySimpleItems(
         uint8 role,
         address _address
     ) public view returns (uint16) {
-        Terms storage terms = TermsByRole(_address, role);
+        Terms storage terms = TermsByRole(role, _address);
         return terms.Simple_Items;
     }
 
-    function UploadSimpleItemsProvider(
-        address _address,
-        uint16 Simple_Items
-    ) public {
-        require(msg.sender == dataProvider);
-        providerMapping[_address].Simple_Items = Simple_Items;
-    }
-
-    function UploadDateProvider(
+    function UploadDate(
+        uint8 role,
         address _address,
         uint16 Start_Year,
         uint16 Start_Month,
         uint16 Start_Day,
         uint16 Months
     ) public {
-        require(msg.sender == dataProvider);
-        providerMapping[_address].Start_Year = Start_Year;
-        providerMapping[_address].Start_Month = Start_Month;
-        providerMapping[_address].Start_Day = Start_Day;
-        providerMapping[_address].Months = Months;
+        Terms storage terms = TermsByRole(role, _address);
+        terms.Start_Year = Start_Year;
+        terms.Start_Month = Start_Month;
+        terms.Start_Day = Start_Day;
+        terms.Months = Months;
     }
 
-    function UploadDateRequester(
+    function UploadAreaCode(
+        uint8 role,
         address _address,
-        uint16 Start_Year,
-        uint16 Start_Month,
-        uint16 Start_Day,
-        uint16 Months
+        uint64 Group_Code,
+        uint8[] memory Country_Code,
+        uint64[] memory Country_Group_Code
     ) public {
-        requesterMapping[_address].Start_Year = Start_Year;
-        requesterMapping[_address].Start_Month = Start_Month;
-        requesterMapping[_address].Start_Day = Start_Day;
-        requesterMapping[_address].Months = Months;
+        if (Country_Code.length != Country_Group_Code.length) {
+            revert(
+                "UploadCountryCode: Country_Code and Country_Group_Code must have the same length"
+            );
+        }
+
+        Terms storage terms = TermsByRole(role, _address);
+
+        if (role == role_provider) {
+            require(msg.sender == dataProvider);
+            for (uint8 i = 0; i < Country_Code.length; i++) {
+                terms.Area_Country_Code_Map[Country_Code[i]] = true;
+            }
+        }
+
+        terms.Area_Country_Code = Country_Code;
+        terms.Area_Country_Group_Code = Country_Group_Code;
+        // terms.Country_Code = Country_Code;
+
+        terms.Area_Group_Code = Group_Code;
     }
 
-    function UploadCountryCodeRequester(
-        address _address,
-        uint32 Group_Code,
-        uint256 Country_Code,
-        uint32 Country_Group_Code
-    ) public {
-        requesterMapping[_address].Country_Code = Country_Code;
-        requesterMapping[_address].Country_Group_Code = Country_Group_Code;
-        requesterMapping[_address].Group_Code = Group_Code;
+    function DisplayAreaCode(
+        uint8 role,
+        address _address
+    ) public view returns (uint64, uint8[] memory, uint64[] memory) {
+        Terms storage terms = TermsByRole(role, _address);
+        return (
+            terms.Area_Group_Code,
+            terms.Area_Country_Code,
+            terms.Area_Country_Group_Code
+        );
     }
 
-    function UploadCountryCodeProvider(
-        address _address,
-        uint32 Group_Code,
-        uint256 Country_Code,
-        uint32 Country_Group_Code
-    ) public {
-        require(msg.sender == dataProvider);
-        providerMapping[_address].Country_Code = Country_Code;
-        providerMapping[_address].Country_Group_Code = Country_Group_Code;
-        providerMapping[_address].Group_Code = Group_Code;
-    }
-
-    function UploadDiseaseCodeRequester(
+    function UploadDiseaseCode(
+        uint8 role,
         address _address,
         uint16[] memory Disease_Code_Array
     ) public {
-        requesterMapping[_address].Disease_Code_Array = Disease_Code_Array;
+        Terms storage terms = TermsByRole(role, _address);
+        for (uint8 i = 0; i < Disease_Code_Array.length; i++) {
+            terms.Disease_Map[Disease_Code_Array[i]] = true;
+        }
     }
 
-    function UploadDiseaseCodeProvider(
-        address _address,
-        uint16[] memory Disease_Code_Array
-    ) public {
-        require(msg.sender == dataProvider);
-        providerMapping[_address].Disease_Code_Array = Disease_Code_Array;
+    function DisplayDiseaseCode(
+        uint8 role,
+        address _address
+    ) public view returns (uint16[] memory) {
+        Terms storage terms = TermsByRole(role, _address);
+        return terms.Disease_Code_Array;
     }
 
     function CheckBooleanItems(
@@ -151,26 +158,38 @@ contract ConsentCode {
         address _provider_address,
         address _requester_address
     ) public view returns (bool) {
-        bool hasValidCountries = (requesterMapping[_requester_address]
-            .Country_Code &
-            providerMapping[_provider_address].Country_Code ==
-            requesterMapping[_requester_address].Country_Code) ||
-            (requesterMapping[_requester_address].Country_Group_Code &
-                providerMapping[_provider_address].Group_Code ==
-                requesterMapping[_requester_address].Country_Group_Code);
-
-        bool hasValidGroupCode = requesterMapping[_requester_address]
-            .Group_Code !=
-            0 &&
-            requesterMapping[_requester_address].Group_Code &
-                providerMapping[_provider_address].Group_Code ==
-            requesterMapping[_requester_address].Group_Code;
-
-        if (hasValidCountries || hasValidGroupCode) {
-            return true;
-        } else {
-            return false;
+        //check countries, countries of requester must be a subset of countries of provider or the group of countries of requester must be a subset of countries of provider
+        for (
+            uint index_requester = 0;
+            index_requester <
+            requesterMapping[_requester_address].Area_Country_Code.length;
+            index_requester++
+        ) {
+            uint8 requester_code = requesterMapping[_requester_address]
+                .Area_Country_Code[index_requester];
+            bool validCountry = providerMapping[_provider_address]
+                .Area_Country_Code_Map[requester_code] ==
+                true ||
+                providerMapping[_provider_address].Area_Group_Code &
+                    requesterMapping[_requester_address]
+                        .Area_Country_Group_Code[index_requester] >
+                0;
+            if (validCountry == false) {
+                return false;
+            }
         }
+
+        // check groups, group of requester must be a subset of group of provider or the group of requester is 0
+        if (
+            requesterMapping[_requester_address].Area_Group_Code == 0 ||
+            providerMapping[_provider_address].Area_Group_Code &
+                requesterMapping[_requester_address].Area_Group_Code ==
+            requesterMapping[_requester_address].Area_Group_Code
+        ) {
+            return true;
+        }
+
+        return false;
     }
 
     function CheckDisease(
